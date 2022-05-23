f0731384019e7e0cd7697d1a9fc5b0403563793a
zhaoyishu <zhaoyishu@yibaogao.com>
2022-05-23 23:58:01 +0800

    【hello world】

--- src/main/java/com/example/demospringboot/controller/TestController.java
+++ src/main/java/com/example/demospringboot/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.example.demospringboot;
+package com.example.demospringboot.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ src/main/java/com/example/demospringboot/service/TestService.java
@@ -0,0 +1,28 @@
+package com.example.demospringboot.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class TestService {
+
+
+/*    顺序表和数组
+    这两个概念及其容易混淆。
+
+    顺序表是线性表的一种实现方式，属于物理结构，顺序表是在计算机内存中以数组的形式保存的线性表
+    数组是一种物理结构，它的存储单元是连续的。
+    由此可见，顺序表相当于是对数组的一次封装，数组是一个更大的概念。
+
+    所以也不用特别计较，大家平时说的时候数组和线性表差不多是一个东西。
+    如果硬要你解释，你就说
+
+    顺序表和数组都是物理结构，顺序表是在计算机内存中以数组的形式保存的线性表*/
+    public void  hashmapTest(){
+        Map<String,Object> map = new HashMap<>();
+
+    }
+
+}
